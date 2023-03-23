a0bb9e21ed103e3e9dfdfa7cf7e8f71c3bbf7225
Roman Kantsir <kantsirroman0313@gmail.com>
2023-03-23 21:03:38 +0200

    Додано сторінку внесення застосунку у БД

--- View/AddAppPage.xaml.cs
+++ View/AddAppPage.xaml.cs
@@ -7,6 +7,9 @@ using System.Windows;
 using System.Windows.Controls;
 using MyApp.ViewModel;
 using System.ComponentModel;
+using System;
+using static System.Net.Mime.MediaTypeNames;
+using System.Windows.Media.Imaging;
 
 namespace MyApp.View
 {
@@ -50,29 +53,52 @@ namespace MyApp.View
                     fileContent = reader.ReadToEnd();
                 }
                 MessageBox.Show(filePath);
-            }
 
+                BitmapImage bitmap = new BitmapImage();
+                bitmap.BeginInit();
+                bitmap.UriSource = new Uri(filePath);
+                bitmap.EndInit();
+                PhotoInPreview.Source = bitmap;
+                
+            }
+            
         }
 
         private void UploadToDB(object sender, RoutedEventArgs e)
         {
             //Загрузили фото в бд
-            if (filePath == string.Empty) {
+            if (filePath == string.Empty)
+            {
                 MessageBox.Show("Додайте фото");
             }
             else
             {
+                string Name = nameInput.Text.ToString();
+                string Company = companyInput.Text.ToString();
+                string About = aboutInput.Text.ToString();
+
+                if (Name != string.Empty && Company != string.Empty && About != string.Empty)
+                {
+                    AppModel newApp = new AppModel(Name, Company, About, filePath);
+                    IUploadPhoto UpPh = new UploadApp();
+                    UpPh.SetUploadApp(newApp);
+                    MessageBox.Show("Додано");
+                }
+                else
+                {
+                    MessageBox.Show("Пусті поля");
+                }
                 //var editableCollectionView = itemsControl.Items as IEditableCollectionView;
 
-                
 
-                /*AppModel model = new AppModel();
-                model.Name = "Roman";
-                IUploadPhoto UpPh = new UploadApp();
-                UpPh.SetUploadApp(model);*/
+                /*  AppModel model = new AppModel();
+                  model.Name = "Roman";*/
             }
         }
 
-        
+        private void Block_SourceUpdated(object sender, System.Windows.Data.DataTransferEventArgs e)
+        {
+
+        }
     }
 }
---
+++ View/AppBlockForAddAppPage.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MyApp.View
+{
+    /// <summary>
+    /// Логика взаимодействия для UserControl1.xaml
+    /// </summary>
+    public partial class AppBlock : UserControl
+    {
+        public int Id;
+        public string Name;
+        public string Company;
+        public string About;
+        public string Image;
+
+
+       /* public string Text
+        {
+            get { return (string)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+
+        public string name
+        {
+            get { return (string)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+
+        public string company
+        {
+            get { return (string)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+
+        }
+
+        public string about
+        {
+            get { return (string)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+
+        }
+
+        public string image
+        {
+            get { return (string)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+
+        }
+*/
+        public AppBlock()
+        {
+            InitializeComponent();
+        }
+    }
+}
